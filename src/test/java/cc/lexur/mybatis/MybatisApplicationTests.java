e25b92902f3bb5553752a087d5aca31773781849
lexur <2259334682@qq.com>
2022-09-10 12:20:33 +0800

    init

---
+++ src/test/java/cc/lexur/mybatis/MybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.lexur.mybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
