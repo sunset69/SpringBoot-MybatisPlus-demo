e80412b83291b6f4a75f5aca2a1c7178e2045401
lexur <2259334682@qq.com>
2022-09-10 15:03:31 +0800

    主程序

--- mybatis-api/src/main/java/cc/lexur/api/MybatisApiApplication.java
+++ mybatis-api/src/main/java/cc/lexur/api/MybatisApiApplication.java
@@ -2,8 +2,10 @@ package cc.lexur.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("cc.lexur") // 这个一定要换成自己的包名
 public class MybatisApiApplication {
 
     public static void main(String[] args) {
